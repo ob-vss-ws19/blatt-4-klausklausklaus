737090e9c59d48e008b7529461de2737e87addc6
HalaKuwatly <halakuwatly@gmail.com>
2020-01-06 22:36:45 +0100

    test

---
+++ movies/movies/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	hello "github.com/blatt-4-klausklausklaus/movies/proto/hello"
+	"github.com/micro/go-micro"
+)
+
+func main() {
+	// create a new service
+	service := micro.NewService()
+
+	// parse command line flags
+	service.Init()
+
+	// Use the generated client stub
+	cl := hello.NewSayService("greeter", service.Client())
+
+	// Make request
+	rsp, err := cl.Hello(context.Background(), &hello.Request{
+		Name: "John",
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(rsp.Msg)
+}
