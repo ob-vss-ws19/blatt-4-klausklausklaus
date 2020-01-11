80edca51bae8f3502db62084249ed3bb8330fc25
HalaKuwatly <halakuwatly@gmail.com>
2020-01-11 14:09:46 +0100

    add movie service

--- movies/main.go
+++ movies/main.go
@@ -2,40 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"time"
-
-	hello "github.com/ob-vss-ws19/blatt-4-klausklausklaus/movies/proto/hello"
 
 	"github.com/micro/go-micro"
-
-	"context"
+	"github.com/ob-vss-ws19/blatt-4-klausklausklaus/movies/movies"
+	proto "github.com/ob-vss-ws19/blatt-4-klausklausklaus/movies/proto"
 )
 
-type Say struct{}
-
-func (s *Say) Hello(ctx context.Context, req *hello.Request, rsp *hello.Response) error {
-	log.Print("Received Say.Hello request")
-	rsp.Msg = "Hello " + req.Name
-	return nil
-}
-
 func main() {
-	service := micro.NewService(
-		micro.Name("greeter"),
-		micro.RegisterTTL(time.Second*30),
-		micro.RegisterInterval(time.Second*10),
-	)
-
-	// optionally setup command line usage
+	service := micro.NewService(micro.Name("movies"))
 	service.Init()
-
-	// Register Handlers
-	hello.RegisterSayHandler(service.Server(), new(Say))
-	fmt.Println("ssssss")
-
-	// Run server
-	if err := service.Run(); err != nil {
-		log.Fatal(err)
+	err1 := proto.RegisterMoviesHandler(service.Server(), movies.CreateNewMoviesHandlerInstance())
+	if err1 == nil {
+		if err := service.Run(); err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println(err1)
 	}
 }
