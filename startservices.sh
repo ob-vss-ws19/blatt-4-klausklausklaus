01562016bd966a5a82b2c1b5c126d18a5ce66e11
malazbw <malazbw@gmail.com>
2020-01-12 19:14:55 +0100

    update startservices

--- startservices.sh
+++ startservices.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "[+] Starting Services"
+echo "Starting Services ..."
 go run ./cinemahall/main.go &
-echo "[+] Started Cinemahall-Service"
+echo "Started Cinemahall-Service ..."
 go run ./movies/main.go &
-echo "[+] Started Movie-Service"
+echo "Started Movie-Service ..."
 go run ./reservation/main.go &
-echo "[+] Started Reservation-Service"
+echo "Started Reservation-Service ..."
 go run ./show/main.go &
-echo "[+] Started Show-Service"
+echo "Started Show-Service ..."
 go run ./users/main.go &
-echo "[+] Started User-Service"
+echo "Started User-Service ..."
